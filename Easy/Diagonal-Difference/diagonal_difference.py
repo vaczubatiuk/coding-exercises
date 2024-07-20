c633ce2d6acc42cc064769a1f92b986fdaf4fa8a
SmashLootRepeat <vaczubatiuk@gmail.com>
2024-07-20 18:31:43 -0500

    h

--- Easy/Diagonal-Difference/diagonal_difference.py
+++ Easy/Diagonal-Difference/diagonal_difference.py
@@ -9,12 +9,23 @@ entry = list()
 # Output: the absolute difference in diagonal values.
 # =========================================================
 def diagonalDifference(arr):
-    # Setting the first 
+    # Setting the left diagonal sum variable 
     leftdiagonal = 0
+    # Setting the right diagonal sum variable
     rightdiagonal = 0
     for i in range(len(arr)):
+        # for left diag [x,y] where x == y
         leftdiagonal += arr[i][i]
+        # for rigth diag [x,y] x == -y-1 as indeces in python lists have negative representations that we can use
+        # to x's corresponding negative index: multiply x by -1 then subtract 1 
+        # or another way to look at it add 1 to x and take it's negative
+        # Example: 
+        # x = 0 -> -1(0) = 0 -> 0-1 = -1
+        # x = 1 -> -1(1) = 1 -> -1-1 = -2
+        # etc.
         rightdiagonal += arr[-i-1][i]
+    # Returning the absoulte value of the difference between 
+    # the left diagonal sum and the right diagonal sum
     return abs(leftdiagonal - rightdiagonal)
 
 
